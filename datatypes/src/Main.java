af28229d7810a6a84742575b85c42e4b110e5dda
tubaps <tuba.97@outlook.com.tr>
2022-09-21 20:22:29 +0300

    Initial commit

---
+++ datatypes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi = 12;
+        sayi = 127;
+
+
+        char karakter = 'A';
+
+        boolean dogruMu=false;
+
+
+    }
+
+}
